128e71489b2b18f80f6185f4cd2ed14afbf3512e
Omar Saad <62719848+Omar-Saad@users.noreply.github.com>
2021-12-21 20:52:35 +0200

    1-Feature scaling 2-Printing confusion matrix

--- input_data.py
+++ input_data.py
@@ -1,14 +1,26 @@
 import pandas as pd
-from numpy import ravel
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 data_set = pd.read_csv("data files/magic04.data", header=None)
 
-# extract features from labels
-features = data_set.iloc[:, :-1]
-labels = data_set.iloc[:, [-1]]
+# extract features and labels
+features = data_set.iloc[:, :-1].values
+labels = data_set.iloc[:, -1].values
+
+# Encoding categorical Labels
+le = LabelEncoder()
+labels = le.fit_transform(labels)
 
 # splitting dataset into 70% for training set and 30% for testing set
+# TODO : make random_state = NONE to genetrae random split every time
 train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.3,
                                                                             random_state=42)
-train_labels = ravel(train_labels)
+
+# Feature scaling using standardization
+# After this step all features will be between -3 and 3
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+train_features = sc.fit_transform(train_features)
+test_features = sc.transform(test_features)
